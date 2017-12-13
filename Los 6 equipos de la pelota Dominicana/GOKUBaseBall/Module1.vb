bce789ff916aea17315615525571ceac55f8bc07
Niguel <34486638+Niguel@users.noreply.github.com>
2017-12-13 01:55:28 -0400

    Inicio del proyecto

---
+++ Los 6 equipos de la pelota Dominicana/GOKUBaseBall/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+
+
+
+        Console.WriteLine("UNIVERSIDAD O&M")
+
+        Console.WriteLine("                 GRUPO:Los peloteros")
+        Console.WriteLine("NOMBRES                  MATRICULAS")
+        Console.WriteLine("NIGUEL ALMONTE.............17-EIIN-1-182")
+        Console.WriteLine("FREDERIN SOTO..............16-SIIN-1-010")
+
+
+
+        Console.WriteLine("LOS 6 EQUIPOS DE LA PELOTA DOMINICANA")
+
+        Console.WriteLine("AGUILAS CIBAÑAS")
+        Console.WriteLine("TIGRES DEL LICEY")
+        Console.WriteLine("3-LEONES DEL ESCOGIDO")
+        Console.WriteLine("4-TOROS DEL ESTE")
+        Console.WriteLine("5-ESTRELLAS ORIENTALES")
+        Console.WriteLine("6-GIGANTES DEL CIBAO")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
